a9d06da19bc7015478dfa5fba61701beb845bcc8
murraycopps <murraycopps@gmail.com>
2023-03-07 11:47:53 -0500

    added time input

---
+++ components/TimeInput.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+export default function TimeInput({
+  time,
+  setTime,
+}: {
+  time: number;
+  setTime: (time: number) => void;
+}) {
+  const [hour, setHour] = useState(Math.floor(time / 3600));
+  const [min, setMin] = useState(Math.floor((time / 60) % 60));
+  const [sec, setSec] = useState(time % 60);
+  useEffect(() => {
+    if (hour < 0) setHour(0);
+    if (min < 0) setMin(0);
+    if (sec < 0) setSec(0);
+    setTime(hour * 3600 + min * 60 + sec);
+  }, [hour, min, sec]);
+
+  useEffect(() => {
+    setHour(Math.floor(time / 3600));
+    setMin(Math.floor((time / 60) % 60));
+    setSec(time % 60);
+  }, [time]);
+  return (
+    <div className="grid grid-cols-3 text-2xl text-black">
+      <div className="relative w-full h-full">
+        <input
+          type="number"
+          placeholder="Hour"
+          name="hour"
+          id="hour"
+          className="w-full px-2 py-4 rounded-l-full"
+          value={hour ? hour : ""}
+          onChange={(event) => {
+            if (event.target.value != "")
+              setHour(parseFloat(event.target.value));
+            else setHour(0);
+          }}
+        />
+        <span className="absolute right-0 z-10 text-4xl bottom-4">:</span>
+      </div>
+      <div className="relative w-full h-full">
+        <input
+          type="number"
+          placeholder="Min"
+          name="min"
+          id="min"
+          className="w-full px-2 py-4"
+          value={min ? min : ""}
+          onChange={(event) => {
+            if (event.target.value != "")
+              setMin(parseFloat(event.target.value));
+            else setMin(0);
+          }}
+        />
+        <span className="absolute right-0 z-10 text-4xl bottom-4">:</span>
+      </div>
+      <input
+        type="number"
+        placeholder="Sec"
+        name="sec"
+        id="sec"
+        className="w-full px-2 py-4 rounded-r-full"
+        value={sec ? sec : ""}
+        onChange={(event) => {
+          if (event.target.value != "") setSec(parseFloat(event.target.value));
+          else setSec(0);
+        }}
+      />
+    </div>
+  );
+}
--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -1,5 +1,11 @@
+import TimeInput from "@/components/TimeInput";
+import { outTime } from "@/scripts";
 import Link from "next/link";
-export default function Tools({ url }: { url: string }) {
+import { useState } from "react";
+
+
+export default function Tools() {
+    const [time, setTime] = useState(0)
     return (
         <div className="flex flex-col items-center gap-4 font-sans text-white bg-gray-800">
             <div className="px-16 py-4 m-4 text-center bg-gray-700 rounded-lg">
@@ -14,6 +20,12 @@ export default function Tools({ url }: { url: string }) {
             <Link href="/tools/counter" className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600">
                 Counter
             </Link>
+
+            <div className="w-1/2">
+                <TimeInput time={time} setTime={setTime} />
+            </div>
+
+            {outTime(time)}
         </div>
     );
 }
