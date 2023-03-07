1a84520cbd3ea1e83d25acae358797c7a9fd2b99
torstenbrickley <torstenbrickley@oldrochester.org>
2023-03-07 13:55:12 -0500

    started shoe.ts

---
+++ pages/reviews/index.tsx
@@ -0,0 +1,20 @@
+export default function Reviews() {
+  return (
+    <div className="flex flex-col h-screen w-screen items-center justify-center gap-4">
+      <img src="/images/reviewlanding.jpg" alt="Reviews" className="fixed top-0 cover bottom-16 opacity-20"/>
+        <h1 className="text-8xl text-orange-500" >Reviews</h1>
+        <div className=" flex flex-row gap-4">
+
+        <a>
+    <p>Reviews page</p>
+
+        </a>
+        <a>
+            <p>Reviews page</p>
+
+        </a>
+        </div>
+
+    </div>
+  );
+}
