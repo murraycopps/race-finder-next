2c8419a0ac1e9a08e09efb4e01cb0aaeb20e05c8
murraycopps <murraycopps@gmail.com>
2023-03-09 08:48:39 -0500

    added tool link

--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -1,57 +1,25 @@
 import PageWrapper from "@/components/PageWrapper";
-import Link from "next/link";
+import ToolLink from "@/components/ToolLink";
 
 export default function Tools() {
   return (
     <PageWrapper
       page="Tools"
-      className="flex flex-col items-center gap-4 font-sans text-white bg-gray-800"
+      className="flex flex-col items-center gap-8 p-4 font-sans text-white bg-gray-800"
     >
-      <div className="px-16 py-4 m-4 text-center bg-gray-700 rounded-lg">
-        <h1 className="text-3xl">Tools</h1>
+      <h1 className="text-5xl">Tools</h1>
+      <p className="text-2xl">
+        A collection of tools to help you with your running.
+      </p>
+      <div className="grid grid-cols-4 gap-4">
+        <ToolLink href="/tools/pacing" name="Pacing" description="Calculate your pace for a given distance." />
+        <ToolLink href="/tools/vdot" name="Equivalent Races" description="Calculate your VDOT score and equivalent times." />
+        <ToolLink href="/tools/unusual" name="Unusual Distances" description="Calculate your pace for unusual distances." />
+        <ToolLink href="/tools/relay" name="Relay Calculator" description="Calculate your team's time for a relay." />
+        <ToolLink href="/tools/scoring" name="Scoring" description="Calculate your score for a track or cross country meet." />
+        <ToolLink href="/tools/speed" name="Speed" description="Convert between speed and pace." />
+        <ToolLink href="/tools/hill" name="Hill Calculator" description="Calculate your pace on a hill." />
       </div>
-      <Link
-        href="/tools/pacing"
-        className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-      >
-        pacing
-      </Link>
-      <Link
-        href="/tools/vdot"
-        className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-      >
-        vdot
-      </Link>
-      <Link
-        href="/tools/unusual"
-        className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-      >
-        unusual
-      </Link>
-      <Link
-        href="/tools/convert"
-        className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-      >
-        convert
-      </Link>
-      <Link
-        href="/tools/hill"
-        className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-      >
-        hill
-      </Link>
-      <Link
-        href="/tools/relay"
-        className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-      >
-        relay
-      </Link>
-      <Link
-        href="/tools/scoring"
-        className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-      >
-        scoring
-      </Link>
     </PageWrapper>
   );
 }
