d2d186d45dde2bac9920b9262edeb3bdd6acfcf3
murraycopps <murraycopps@gmail.com>
2023-03-09 09:34:23 -0500

    styled tools home page

--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -5,20 +5,51 @@ export default function Tools() {
   return (
     <PageWrapper
       page="Tools"
-      className="flex flex-col items-center gap-8 p-4 font-sans text-white bg-gray-800"
+      className="flex flex-col items-center min-h-screen gap-8 p-4 font-sans text-white bg-gray-800 justify-evenly"
     >
-      <h1 className="text-5xl">Tools</h1>
-      <p className="text-2xl">
-        A collection of tools to help you with your running.
+      <h1 className="text-6xl">Tools</h1>
+      <p className="w-3/4 text-2xl text-center">
+        Our website offers a range of online tools for runners, including time
+        conversion tools for track and cross-country, aiding progress tracking
+        and performance comparison for runners of all levels. Try them out now
+        and achieve your goals like a pro!
       </p>
-      <div className="grid grid-cols-4 gap-4">
-        <ToolLink href="/tools/pacing" name="Pacing" description="Calculate your pace for a given distance." />
-        <ToolLink href="/tools/vdot" name="Equivalent Races" description="Calculate your VDOT score and equivalent times." />
-        <ToolLink href="/tools/unusual" name="Unusual Distances" description="Calculate your pace for unusual distances." />
-        <ToolLink href="/tools/relay" name="Relay Calculator" description="Calculate your team's time for a relay." />
-        <ToolLink href="/tools/scoring" name="Scoring" description="Calculate your score for a track or cross country meet." />
-        <ToolLink href="/tools/speed" name="Speed" description="Convert between speed and pace." />
-        <ToolLink href="/tools/hill" name="Hill Calculator" description="Calculate your pace on a hill." />
+      <div className="flex flex-row flex-wrap items-center justify-center gap-4">
+        <ToolLink
+          href="/tools/pacing"
+          name="Pacing"
+          description="Calculate your running pace for any distance and plan your training effectively."
+        />
+        <ToolLink
+          href="/tools/vdot"
+          name="Equivalent Races"
+          description="Estimate race performance using VDOT score to calculate equivalent times."
+        />
+        <ToolLink
+          href="/tools/unusual"
+          name="Unusual Distances"
+          description="Calculate your running pace for non-standard distances."
+        />
+        <ToolLink
+          href="/tools/relay"
+          name="Relay Calculator"
+          description="Calculate your team's time for a relay by adding any number of times together."
+        />
+        <ToolLink
+          href="/tools/scoring"
+          name="Scoring"
+          description="Calculate your score for a track or cross country meet."
+        />
+        <ToolLink
+          href="/tools/convert"
+          name="Convert"
+          description="Convert between different units of measurement."
+        />
+        <ToolLink
+          href="/tools/hill"
+          name="Hill Calculator"
+          description="Calculate your running pace on hills using change in elevation."
+        />
       </div>
     </PageWrapper>
   );
