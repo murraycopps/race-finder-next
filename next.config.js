6e322a475d551e2cf0e65823a29ea1155bd7c9ae
murraycopps <murraycopps@gmail.com>
2023-03-09 10:31:38 -0500

    configured the config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dgalywyr863hv.cloudfront.net',
+        port: '',
+        pathname: '/pictures/athletes/*/*/*/*',
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
