2f6602abc6156b42c8eaab6adcc1405ad2088491
torstenbick <torstenbrickley@oldrochester.org>
2023-03-13 12:38:08 -0400

    finsihed shoe info page

--- pages/reviews/shoe/[pid].tsx
+++ pages/reviews/shoe/[pid].tsx
@@ -23,11 +23,11 @@ export default function ShoesPage() {
 
     return (
 
-        <PageWrapper page={shoe?.name || "Shoe"} className="flex flex-col justify-center item-center min-h-screen mx-32 my-4">
+        <PageWrapper page={shoe?.name || "Shoe"} className="flex flex-col justify-center min-h-screen mx-32 my-4 item-center">
             {shoe ? (
-                <div className="grid grid-cols-3 h-full w-4/5 p-4 m-4 bg-slate-400 rounded-lg min-w-fit">
-                    <div className="shoe-display">
-                        <h1 className="name">{shoe.name}</h1>
+                <div className="grid w-4/5 h-full p-4 m-4 rounded-lg temp-cols-3 bg-slate-400 min-w-fit">
+                    <div className="flex flex-col justify-center p-4 m-1 font-medium leading-6 text-white item-center align-center">
+                        <h1 className="bg-slate-400 rounded-lg p-0.5 w-full">{shoe.name}</h1>
                         <img
                             src={shoe.img}
                             alt={shoe.name}
@@ -35,12 +35,12 @@ export default function ShoesPage() {
                             height="400"
                         />
 
-                        <div className="shoe-buy">
-                            <h2 className="text-center m-0 p-0 text-3xl">Purchase</h2>
-                            <div className="purchase-link">
-                                <a className="hover" href={shoe.link}>
+                        <div className="grid justify-center p-4 m-4 text-lg font-medium leading-6 text-center text-white rounded-lg bg-slate-400">
+                            <h2 className="p-0 m-0 text-3xl text-center">Purchase</h2>
+                            <div className="flex justify-center gap-2">
+                                <a className="hover:opacity-50" href={shoe.link}>
                                     <img
-                                        className="img-hov"
+                                        className="block overflow-hidden bg-transparent rounded-md opacity-100 transition-background"
                                         src="https://media.licdn.com/dms/image/C4E0BAQHBmWI9w3tzog/company-logo_200_200/0/1544572615288?e=2147483647&v=beta&t=HhwAog-YBLZyc1ULuVv48MZefPUw3EHejmW25OtMwUQ"
                                         alt="running warehouse"
                                         width="100"
@@ -48,9 +48,9 @@ export default function ShoesPage() {
                                     />
                                 </a>
 
-                                <a className="hover" href={shoe.brandLink}>
+                                <a className="hover:opacity-50" href={shoe.brandLink}>
                                     <img
-                                        className="img-hov"
+                                        className="block overflow-hidden bg-transparent rounded-md opacity-100 transition-background"
                                         src={shoe.brandLogo}
                                         alt={shoe.brand}
                                         width="100"
@@ -61,54 +61,54 @@ export default function ShoesPage() {
                         </div>
                     </div>
 
-                    <div className=" cols-span-2">
-                        <div className="shoe-specs">
-                            <div className="flex flex-col justify-evenly items-center gap-2">
-                                <p><span className="font-medium text-lg">Brand:</span></p>
-                                <p className="font-bold text-lg">{shoe.brand}</p>
+                    <div className="grid p-4 temp-rows-5 cols-span-2">
+                        <div className="flex-wrap justify-center gap-5 p-4 m-4 text-xl font-medium leading-6 text-center text-white border-4 border-solid rounded-lg specs bg-slate-400 border-dark">
+                            <div className="flex flex-col items-center gap-2 justify-evenly">
+                                <p><span className="text-lg font-medium">Brand:</span></p>
+                                <p className="text-lg font-bold">{shoe.brand}</p>
                             </div>
-                            <div className="flex flex-col justify-evenly items-center gap-2">
-                                <p><span className="font-medium text-lg">Price:</span></p>
-                                <p className="font-bold text-lg">${shoe.price}</p>
+                            <div className="flex flex-col items-center gap-2 justify-evenly">
+                                <p><span className="text-lg font-medium">Price:</span></p>
+                                <p className="text-lg font-bold">${shoe.price}</p>
                             </div>
-                            <div className="flex flex-col justify-evenly items-center gap-2">
-                                <p><span className="font-medium text-lg">Best Use:</span></p>
-                                <p className="font-bold text-lg">{shoe.bestUse}</p>
+                            <div className="flex flex-col items-center gap-2 justify-evenly">
+                                <p><span className="text-lg font-medium">Best Use:</span></p>
+                                <p className="text-lg font-bold">{shoe.bestUse}</p>
                             </div>
-                            <div className="flex flex-col justify-evenly items-center gap-2">
-                                <p><span className="font-medium text-lg">Surface:</span></p>
-                                <p className="font-bold text-lg">{shoe.surface}</p>
+                            <div className="flex flex-col items-center gap-2 justify-evenly">
+                                <p><span className="text-lg font-medium">Surface:</span></p>
+                                <p className="text-lg font-bold">{shoe.surface}</p>
                             </div>
-                            <div className="flex flex-col justify-evenly items-center gap-2">
-                                <p><span className="font-medium text-lg">Rating:</span></p>
-                                <p className="font-bold text-lg">
+                            <div className="flex flex-col items-center gap-2 justify-evenly">
+                                <p><span className="text-lg font-medium">Rating:</span></p>
+                                <p className="text-lg font-bold">
                                     <span className="stars">{shoe.rating}</span>
                                 </p>
                             </div>
-                            <div className="flex flex-col justify-evenly items-center gap-2">
-                                <p><span className="font-medium text-lg">Drop:</span></p>
-                                <p className="font-bold text-lg">{shoe.drop}mm</p>
+                            <div className="flex flex-col items-center gap-2 justify-evenly">
+                                <p><span className="text-lg font-medium">Drop:</span></p>
+                                <p className="text-lg font-bold">{shoe.drop}mm</p>
                             </div>
-                            <div className="flex flex-col justify-evenly items-center gap-2">
-                                <p><span className="font-medium text-lg">Weight:</span></p>
-                                <p className="font-bold text-lg">{shoe.weight}g</p>
+                            <div className="flex flex-col items-center gap-2 justify-evenly">
+                                <p><span className="text-lg font-medium">Weight:</span></p>
+                                <p className="text-lg font-bold">{shoe.weight}g</p>
                             </div>
-                            <div className="flex flex-col justify-evenly items-center gap-2">
-                                <p><span className="font-medium text-lg"> Heel Stack Height:</span></p>
-                                <p className="font-bold text-lg">{shoe.heelStackHeight}mm</p>
+                            <div className="flex flex-col items-center gap-2 justify-evenly">
+                                <p><span className="text-lg font-medium"> Heel Stack Height:</span></p>
+                                <p className="text-lg font-bold">{shoe.heelStackHeight}mm</p>
                             </div>
-                            <div className="flex flex-col justify-evenly items-center gap-2">
-                                <p><span className="font-medium text-lg"> Forefoot Stack Height:</span></p>
-                                <p className="font-bold text-lg">{shoe.forefootStackHeight}mm</p>
+                            <div className="flex flex-col items-center gap-2 justify-evenly">
+                                <p><span className="text-lg font-medium"> Forefoot Stack Height:</span></p>
+                                <p className="text-lg font-bold">{shoe.forefootStackHeight}mm</p>
                             </div>
-                            <div className="flex flex-col justify-evenly items-center gap-2">
-                                <p><span className="font-medium text-lg"> Shoe Type:</span></p>
-                                <p className="font-bold text-lg">{shoe.arch}</p>
+                            <div className="flex flex-col items-center gap-2 justify-evenly">
+                                <p><span className="text-lg font-medium"> Shoe Type:</span></p>
+                                <p className="text-lg font-bold">{shoe.arch}</p>
                             </div>
                         </div>
-                        <div className="shoe-rewiew">
-                            <h2 className="text-center m-0 p-0 text-3xl">Review</h2>
-                            <p className="size-lg font-medium text-center">{shoe.review}</p>
+                        <div className="grid justify-center p-4 m-4 text-xl font-medium leading-6 text-center text-white border-4 border-solid rounded-lg temp-rows bg-slate-400 border-dark">
+                            <h2 className="p-0 m-0 text-3xl text-center">Review</h2>
+                            <p className="font-medium text-center size-lg">{shoe.review}</p>
                         </div>
                     </div>
                 </div>
