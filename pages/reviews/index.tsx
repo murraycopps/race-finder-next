25773609c3554750172d5e25b859fac5f981a693
torstenbrickley <torstenbrickley@oldrochester.org>
2023-03-15 08:47:54 -0400

    worked on shoe page

--- pages/reviews/index.tsx
+++ pages/reviews/index.tsx
@@ -13,7 +13,7 @@ import {clothes} from "@/scripts/clothes";
 
   return (
     <PageWrapper page="Reviews" className="flex flex-col items-center justify-start w-screen h-screen gap-4">
-      {/* <img src="/images/review-landing.jpg" alt="Reviews" className="object-cover w-screen h-2/3 bottom-16 opacity-20"/> */}
+       <img src="/images/review-landing.jpg" alt="Reviews" className="object-cover w-screen h-3/4 bottom-16 opacity-20"/>
         <h1 className="mt-8 text-6xl text-red-300"  >Reviews 🐸</h1>
         <div className="grid justify-start w-full grid-cols-1 gap-4 p-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
             {
