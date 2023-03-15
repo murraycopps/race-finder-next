3c4a36df9ff71095ae4a02aa5dbb64d6f3d6261f
torstenbick <torstenbrickley@oldrochester.org>
2023-03-15 10:20:01 -0400

    added page name

---
+++ pages/reviews/clothes/index.tsx
@@ -0,0 +1,9 @@
+import PageWrapper from "@/components/PageWrapper";
+
+export default function Reviews() {
+return (
+<PageWrapper page="Clothes" className="">
+    <h1>Reviews</h1>
+</PageWrapper>
+)
+}
