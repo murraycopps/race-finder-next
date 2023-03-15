46b72727e9e4d887a869f6607be04418c5cb4e0b
murraycopps <murraycopps@gmail.com>
2023-03-15 11:47:45 -0400

    added routing cards

--- components/Reviews/ReviewsRoutingCard.tsx
+++ components/Reviews/ReviewsRoutingCard.tsx
@@ -1,4 +1,28 @@
-import {Item} from "@/scripts/types";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function ReviewsRoutingCard() {
+const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export default function ShopLink({
+  type,
+  image,
+}: {
+  type: string;
+  image: string;
+}) {
+  return (
+    <Link href={`/shop/${type}`} className="relative w-full aspect-square">
+      <Image
+        src={image}
+        alt={type}
+        className="object-cover bg-green-200 rounded-3xl aspect-square w-full filter blur-[0.4rem] brightness-[0.65]"
+        width={500}
+        height={500}
+      />
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-4 p-4 text-white">
+        <h2 className="text-5xl">{capitalize(type)}</h2>
+        <p className="text-2xl">Shop our selection of {type}</p>
+      </div>
+    </Link>
+  );
 }
--- pages/reviews/index.tsx
+++ pages/reviews/index.tsx
@@ -1,32 +1,40 @@
 import PageWrapper from "@/components/PageWrapper";
 import ShoeCard from "@/components/Reviews/ShoeCard";
-import {shoes} from "@/scripts/shoes";
-import {useEffect, useState} from "react";
+import { shoes } from "@/scripts/shoes";
+import { useEffect, useState } from "react";
 import { Item } from "@/scripts/types";
 import ItemCard from "@/components/Reviews/ItemCard";
-import {clothes} from "@/scripts/clothes";
-    export default function Reviews() {
-        const [randomItems, setRandomItems] = useState([] as Item[]);
-        useEffect(() => {
-            setRandomItems([...shoes,...clothes].sort((a, b) => Math.random()-0.5).slice(0, 6))
-        }, [])
+import { clothes } from "@/scripts/clothes";
+import ReviewsRoutingCard from "@/components/Reviews/ReviewsRoutingCard";
+export default function Reviews() {
+  const [randomItems, setRandomItems] = useState([] as Item[]);
+  useEffect(() => {
+    setRandomItems(
+      [...shoes, ...clothes].sort((a, b) => Math.random() - 0.5).slice(0, 6)
+    );
+  }, []);
 
   return (
-    <PageWrapper page="Reviews" className="flex flex-col items-center justify-start w-screen h-screen gap-4">
-       <img src="/images/review-landing.jpg" alt="Reviews" className="object-cover w-screen h-3/4 bottom-16 opacity-20"/>
-        <h1 className="mt-8 text-6xl text-red-300"  >Reviews 🐸</h1>
-        <div className="grid justify-start w-full grid-cols-1 gap-4 p-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
-            {
-                randomItems
-                    .slice(0,4)
-                    .map((item,i ) => {
-                    return (
-                        <ItemCard item={item} key={i}/>
-                    )
-                })
-            }
-        </div>
-
+    <PageWrapper
+      page="Reviews"
+      className="flex flex-col items-center justify-start w-screen h-screen gap-4"
+    >
+      <img
+        src="/images/review-landing.jpg"
+        alt="Reviews"
+        className="object-cover w-screen h-3/4 bottom-16 opacity-20"
+      />
+      <h1 className="mt-8 text-6xl text-red-300">Reviews 🐸</h1>
+      <div className="grid w-full grid-cols-1 gap-8 px-8 sm:grid-cols-2 md:grid-cols-3">
+        <ReviewsRoutingCard type="shoes" image="/review-card/shoe.jpg" />
+        <ReviewsRoutingCard type="gear" image="/review-card/gear.jpg" />
+        <ReviewsRoutingCard type="clothes" image="/review-card/clothes.jpg" />
+      </div>
+      <div className="grid justify-start w-full grid-cols-1 gap-4 p-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+        {randomItems.slice(0, 4).map((item, i) => {
+          return <ItemCard item={item} key={i} />;
+        })}
+      </div>
     </PageWrapper>
   );
 }
