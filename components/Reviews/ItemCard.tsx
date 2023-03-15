c4849abb4658c3ba45dcc866863dca0a00542ab0
murraycopps <murraycopps@gmail.com>
2023-03-15 12:21:51 -0400

    working on images

--- components/Reviews/ItemCard.tsx
+++ components/Reviews/ItemCard.tsx
@@ -5,17 +5,17 @@ import Stars from "./Stars";
 export default function Reviews({item} : {item: Item}) {
 
     return (
-        <section className="w-full text-white rounded-lg bg-slate-400 w-card">
-            <div className="flex items-center m-5">
-                <Link className="w-full no-underline " href={`/reviews/${item.type}/${item.id}`}>
-                    <div className="item">
-                        <div className="flex-col items-center mx-auto my-0">
-                            <h2 className="itemblockTitle">{item.name}</h2>
+        // <section className="w-full text-white rounded-lg bg-slate-400 w-card">
+        //     <div className="flex items-center m-5">
+                <Link className="w-full p-4 no-underline rounded-lg bg-slate-400 w-card" href={`/reviews/${item.type}/${item.id}`}>
+                    {/* <div className="item"> */}
+                        <div className="flex-col items-center w-full mx-auto my-0">
+                            <h2 className="text-xl text-center truncate itemblockTitle text-ellipsis">{item.name}</h2>
                             <img className={` w-full object-cover ${item.type}`} src={item.img} alt={item.name} height="300" width="300"/>
                         </div>
-                        <span className="box-content block w-full h-0 m-2 border-solid border-y-2 border-dark"/>
 
-                        <div className="flex w-full justify-evenly">
+                        <div className="flex flex-wrap w-full justify-evenly">
+                        <span className="box-content block w-full h-0 m-2 border-solid border-y-2 border-dark"/>
                             <div>
                                 <p><span className="text-xs font-bold">Price:</span></p>
                                 <p className="m-0 text-xl ">${item.price}</p>
@@ -28,9 +28,9 @@ export default function Reviews({item} : {item: Item}) {
                                 </p>
                             </div>
                         </div>
-                    </div>
+                    {/* </div> */}
                 </Link>
-            </div>
-        </section>
+        //     </div>
+        // </section>
     )
 }
--- pages/reviews/index.tsx
+++ pages/reviews/index.tsx
@@ -1,11 +1,11 @@
 import PageWrapper from "@/components/PageWrapper";
-import ShoeCard from "@/components/Reviews/ShoeCard";
 import { shoes } from "@/scripts/shoes";
 import { useEffect, useState } from "react";
 import { Item } from "@/scripts/types";
 import ItemCard from "@/components/Reviews/ItemCard";
 import { clothes } from "@/scripts/clothes";
 import ReviewsRoutingCard from "@/components/Reviews/ReviewsRoutingCard";
+import Image from "next/image";
 export default function Reviews() {
   const [randomItems, setRandomItems] = useState([] as Item[]);
   useEffect(() => {
@@ -19,11 +19,14 @@ export default function Reviews() {
       page="Reviews"
       className="flex flex-col items-center justify-start w-screen h-screen gap-4"
     >
-      <img
-        src="/images/review-landing.jpg"
-        alt="Reviews"
-        className="object-cover w-screen h-3/4 bottom-16 opacity-20"
-      />
+        <Image
+          src="/images/review-landing.jpg"
+          alt="Reviews"
+          className="object-cover w-full h-75vh opacity-20"
+            width={1920}
+            height={1080}
+        />
+
       <h1 className="mt-8 text-6xl text-red-300">Reviews 🐸</h1>
       <div className="grid w-full grid-cols-1 gap-8 px-8 sm:grid-cols-2 md:grid-cols-3">
         <ReviewsRoutingCard type="shoes" image="/review-cards/shoe.webp" />
