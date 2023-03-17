1a477b77f5139a4613faec3c8a8e3894e5516a46
torstenbrickley <torstenbrickley@oldrochester.org>
2023-03-17 10:42:00 -0400

    started other.ts

---
+++ scripts/other.ts
@@ -0,0 +1,24 @@
+import {Clothes} from "@/scripts/types";
+
+const other[] = [
+    {
+        name: 'Garmin forerunner 955',
+        price: 499.99,
+        rating: 5,
+        img: "/images/vaporfly.png",
+        color: "Black",
+        use: "Training",
+        battery: 15,
+        brand: "Garmin",
+        brandLogo: "/images/boa-logo.png",
+        link: "https://www.runningwarehouse.com/Mens_Nike_Vaporfly/catpage-MVAPORFLY.html",
+        brandLink: "https://www.garmin.com/en-US/p/777655",
+        review: "stuff",
+        id: 'garmin-forerunner-955',
+        type: "other",
+        otherType: "watch"
+    }
+
+
+    ]
+export {other};
