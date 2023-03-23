b05aee7e8664ac5b58c28ad3ee6702eb16afbda2
murraycopps <murraycopps@gmail.com>
2023-03-23 10:52:57 -0400

    pasted shoe list

---
+++ components/ShoeList.tsx
@@ -0,0 +1,55 @@
+const shoes = [
+    {
+        distance: 'Track Distance',
+        shoes: [
+            { shoe: 'ZoomX Dragonfly', brand: 'nike' },
+            { shoe: 'Air Zoom Victory', brand: 'nike' },
+            { shoe: 'Adizero Avanti TYO', brand: 'adidas' },
+            { shoe: 'FuelCell LD', brand: 'new' },
+            { shoe: 'Cielo X 2 LD', brand: 'hoka' }
+        ]
+    },
+    {
+        distance: 'Track Intervals',
+        shoes: [
+            { shoe: 'ZoomX Streakfly', brand: 'nike' },
+            { shoe: 'ZoomX Vaporfly Next% 2', brand: 'nike' },
+            { shoe: 'Adizero Takumi Sen 9', brand: 'adidas' },
+            { shoe: 'ZoomX Vaporfly 3', brand: 'nike' },
+            { shoe: 'MetaSpeed Sky', brand: 'asics' }
+        ]
+    },
+    {
+        distance: '5k/10k',
+        shoes: [
+            { shoe: 'ZoomX Vaporfly Next% 2', brand: 'nike' },
+            { shoe: 'Adizero Takumi Sen 9', brand: 'adidas' },
+            { shoe: 'ZoomX Vaporfly Next% 3', brand: 'nike' },
+            { shoe: 'MetaSpeed Sky+', brand: 'asics' },
+            { shoe: 'ZoomX Streakfly', brand: 'nike' }
+        ]
+    },
+    {
+        distance: 'Half Marathon',
+        shoes: [
+            { shoe: 'ZoomX Vaporfly Next% 3', brand: 'nike' },
+            { shoe: 'ZoomX Vaporfly Next% 2', brand: 'nike' },
+            { shoe: 'ZoomX Alphafly Next%', brand: 'nike' },
+            { shoe: 'ZoomX Alphafly Next% 2', brand: 'nike' },
+            { shoe: 'MetaSpeed Sky+', brand: 'asics' }
+        ]
+    },
+    {
+        distance: 'Marathon',
+        shoes: [
+            { shoe: 'ZoomX Alphafly Next% 2', brand: 'nike' },
+            { shoe: 'ZoomX Alphafly Next%', brand: 'nike' },
+            { shoe: 'ZoomX Vaporfly Next% 3', brand: 'nike' },
+            { shoe: 'ZoomX Vaporfly Next% 2', brand: 'nike' },
+            { shoe: 'Adizero Pro 3', brand: 'adidas' }
+        ]
+    }
+];
+
+
+export default function ShoeList() {
