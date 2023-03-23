f38162bbcb2ed3c2d1a86be2e4bf0ba1900b561d
murraycopps <murraycopps@gmail.com>
2023-03-23 11:17:39 -0400

    added images and finished shoe list

--- components/ShoeList.tsx
+++ components/ShoeList.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import styles from "@/styles/ShoeList.module.css";
+
 const shoes = [
     {
         distance: 'Track Distance',
@@ -52,4 +55,106 @@ const shoes = [
 ];
 
 
-export default function ShoeList() {}
+export default function ShoeList() {
+    const [index, setIndex] = useState(0);
+    const [left, setLeft] = useState(false);
+    const [right, setRight] = useState(false);
+    const [pastHalf, setPastHalf] = useState(false);
+    return(
+        <div
+	className={`flex flex-col items-center float-left gap-4 p-4 ml-2 mr-12 overflow-x-hidden text-white ${styles.width} rounded-2xl`}
+>
+	<h2 className="text-4xl font-bold text-center">Fastest Shoes</h2>
+	<div className="flex flex-row items-center justify-center gap-2">
+		<button
+			onClick={() => {
+				if (left || right) return;
+				setLeft(true);
+				setTimeout(() => {
+					setPastHalf(true);
+				}, 500);
+				setTimeout(() => {
+                    setLeft(false);
+                    setPastHalf(false);
+					if (index > 0) setIndex(index - 1);
+					else setIndex(shoes.length - 1);
+				}, 1000);
+			}}
+		>
+			<svg
+				className="w-12 h-12 scale-150 rotate-90"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M7 10L12 15L17 10"
+					stroke="white"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+				/>
+			</svg>
+		</button>
+
+		<p className={`w-64 text-4xl text-center ${(left || right) && styles['fade-change']}`}>
+			{shoes[pastHalf ? (left ? (index > 0? index - 1 : shoes.length - 1) : (index < shoes.length - 1 ? index + 1 : 0)) : index].distance}
+		</p>
+
+		<button
+			onClick={() => {
+				if (left || right) return;
+				setRight(true);
+				setTimeout(() => {
+					setPastHalf(true);
+				}, 500);
+				setTimeout(() => {
+					setRight(false);
+					setPastHalf(false);
+					if (index < shoes.length - 1) setIndex(index + 1);
+					else setIndex(0);
+				}, 1000);
+			}}
+		>
+			<svg
+				className="w-12 h-12 scale-150 -rotate-90"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M7 10L12 15L17 10"
+					stroke="white"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+				/>
+			</svg>
+		</button>
+	</div>
+	<div className={`${styles.display} grid place-items-center ${left && styles.left} ${right && styles.right}`}>
+		<ul className="flex flex-col gap-2 text-2xl w-fit">
+			{shoes.at(index - 1)?.shoes.map((shoe, i) => (
+				<li className={`${styles[shoe.brand]} ${styles.shoe}`} key={i}>
+					{shoe.shoe}
+				</li>
+			))}
+		</ul>
+		<ul className="flex flex-col gap-2 text-2xl">
+			{shoes[index]?.shoes.map((shoe, i) => (
+				<li className={`${styles[shoe.brand]} ${styles.shoe}`} key={i}>
+					{shoe.shoe}
+				</li>
+)        )}
+		</ul>
+		<ul className="flex flex-col gap-2 text-2xl">
+			{shoes.at(index < shoes.length - 1 ? index + 1 : 0)?.shoes.map((shoe, i) => (
+				<li className={`${styles[shoe.brand]} ${styles.shoe}`} key={i}>
+					{shoe.shoe}
+				</li>
+			))}
+		</ul>
+	</div>
+</div>
+    )
+}
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import PageWrapper from "@/components/PageWrapper";
+import ShoeList from "@/components/ShoeList";
 
 export default function HomePage() {
     return (
-        <PageWrapper page="Home" className="flex flex-col gap-16 p-16 min-h-screen items-center text-center">
+        <PageWrapper page="Home" className="flex flex-col items-center min-h-screen gap-16 p-16 text-center">
                 <h1 className="text-4xl font-bold">Welcome to my website!</h1>
-                <div className="grid grid-cols-1 gap-8 sm:grid-cols-3 w-full">
+                <div className="grid w-full grid-cols-1 gap-8 sm:grid-cols-3">
                     <Card
                         title="Strava"
                         description="Connect to Strava and view your stats"
@@ -26,6 +27,7 @@ export default function HomePage() {
                         img="/review-cards/shoe.webp"
                     />
                 </div>
+                <ShoeList />
 
         </PageWrapper>
 
@@ -42,10 +44,10 @@ type CardProps = {
 }
 
 const Card = ({title, description, link, img}: CardProps) => (
-    <Link href={link} className="flex flex-col relative justify-evenly rounded-3xl bg-strava p-8 gap-8 aspect-square overflow-hidden">
-        <Image src={img} alt="" width="1000" height="1000" className="absolute inset-0 w-full h-ful object-cover" />
-        <div className="absolute inset-0 w-full h-full bg-gray-600 opacity-90" />
-        <h2 className="text-4xl font-bold z-50">{title}</h2>
-        <p className="text-2xl z-50">{description}</p>
+    <Link href={link} className="relative flex flex-col gap-8 p-8 overflow-hidden justify-evenly rounded-3xl bg-strava aspect-square">
+        <Image src={img} alt="" width="1000" height="1000" className="absolute inset-0 object-cover w-full h-ful" />
+        <div className="absolute inset-0 w-full h-full bg-gray-600 opacity-80 hover:opacity-70" />
+        <h2 className="z-50 text-4xl font-bold">{title}</h2>
+        <p className="z-50 text-2xl">{description}</p>
     </Link>
 )
