5721025d504262f48e2df14b8d2950e5f4f0ff39
torstenbrickley <torstenbrickley@oldrochester.org>
2023-03-29 13:04:42 -0400

    images

--- scripts/quotes.ts
+++ scripts/quotes.ts
@@ -4,12 +4,12 @@ const quotes:Quote[] = [
     {
         name: "Eliud Kipchoge",
         quote: "No Human is Limited",
-        img: "/images/Kip.jpeg"
+        img: "/images/quotes/Kip.jpeg"
     },
     {
         name: "Steve Prefontaine",
         quote: "To give anything less than your best, is to sacrifice the gift.",
-        img: "/images/Pre.jpeg"
+        img: "/images/quotes/Pre.jpeg"
     }
     ];
 
