5ae8052252a192a0e928c5f3c51a679b1136f071
torstenbick <torstenbrickley@oldrochester.org>
2023-03-29 12:44:51 -0400

    qurotes relateve

--- components/HomePage/QuoteCard.tsx
+++ components/HomePage/QuoteCard.tsx
@@ -9,8 +9,8 @@ export default function QuotesCard() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-between w-full h-full gap-8 p-4 rounded-3xl bg-faded-purple-600">
-      <img className="object-cover w-32 h-32 rounded-full" src={quote.img} alt=""/>
+    <div className="relative flex flex-col items-center justify-between w-full h-full gap-8 p-4 rounded-3xl bg-faded-purple-600">
+      <img className="absolute object-cover w-32 h-32 rounded-full" src={quote.img} alt=""/>
       <p className="text-xl font-bold quote ">{quote.quote}</p>
       <p className="text-lg font-bold">-{quote.name}</p>
     </div>
