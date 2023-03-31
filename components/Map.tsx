92ed8c0e8192f561fdc98524de47b30b20371c7a
murraycopps <murraycopps@gmail.com>
2023-03-31 12:37:10 -0400

    added prop

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,4 +1,6 @@
-// import { MapContainer, Marker, TileLayer, Polyline } from "react-leaflet";
+import { useState, useEffect } from 'react';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, useMap, Polyline } from 'react-leaflet';
 
 const polyline = require("polyline-encoded");
 
@@ -12,33 +14,6 @@ function parseMapPoints(map: any) {
   return mapPoints;
 }
 
-// const Map = ({ map }: { map: any }) => {
-
-//   const positions = parseMapPoints(map);
-
-//   return (
-//     <MapContainer center={positions[0]} zoom={13} style={{ height: '100vh' }}>
-//       <TileLayer
-//         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//       />
-//       <Marker position={positions[0]} />
-//       {/* <Polyline positions={positions} color="blue" weight={5} /> */}
-//     </MapContainer>
-//   );
-// };
-
-// export default Map;
-
-const myMap = {
-  "id": "a8793724789",
-  "summary_polyline": "slj}Fn}~nLJOB@@GES?a@[eABUDA`@DNSRi@VQ~Ao@TOXa@Z_Ah@iAhCLn@Gh@Up@_@zBiAVKl@Qf@?XHJDb@^^Vf@h@l@x@p@l@Zf@\\`@rBvBb@TJAPMRc@Pi@|@{C^yBx@gDLkALk@NeAFaBW{E[cCQ{Bi@wDG_A[gCE}@@y@Nk@JSd@a@xAwBHEZc@j@{@|@mA\\[`@Sx@g@x@}@VI~AgAx@e@LQ~@eBn@k@f@U~@WnAWp@E\\BRDr@Sx@EpCc@x@ChC]xAe@h@@b@GL?t@Id@Cj@K`CWVKZAt@Oj@S^S\\_@Dm@@q@Iq@Ae@@eDEW@aAG{@CuCCwD@]DMRQ`@EfDFnDA^DqAQyCFcCFi@DYFIJE`@JvBCtBHdA?rAH|A@lBGxABRFbBKh@CBa@V_A\\mBRa@PI@m@De@JgAHSDiFh@aC`@e@De@@[DOF@Bc@A_@D}@Tw@LgBBOJSBQ?iATo@Ti@Xc@^[\\KPa@j@c@`@qBvAc@d@_@Zy@h@mAbA_@b@e@x@gArAk@~@}A`BUp@CXBXLv@\\pAd@rDNt@TjCHZBXA?@SECF^@vAPhBDbBDp@AhAOjBa@zAKr@S|@g@pCIt@{BnFUROSQq@_@[kBqBYe@k@i@k@w@qBwA]Qa@Ck@JuC~AqB`Aa@Dm@C",
-  "resource_state": 2
-};
-
-import { useState, useEffect } from 'react';
-import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, useMap, Polyline } from 'react-leaflet';
 
 export function ChangeView({ coords }: {coords: any}) {
   const map = useMap();
@@ -46,14 +21,9 @@ export function ChangeView({ coords }: {coords: any}) {
   return null;
 }
 
-const geoData = {
-  lat: parseMapPoints(myMap)[0].lat,
-  lng: parseMapPoints(myMap)[0].lng,
-};
-const positions = parseMapPoints(myMap);
-
-export default function Map() {
+export default function Map({map} : {map: any}) {
   // const [geoData, setGeoData] = useState({ lat:g, lng: 16.779852 });
+  const positions = parseMapPoints(map);
 
 // const center is average of all points
   const center = {
--- pages/home.tsx
+++ pages/home.tsx
@@ -98,7 +98,7 @@ export default function HomePage() {
       </div>
 
       <IntensityCard />
-        <MapWithNoSSR />
+        <MapWithNoSSR map={myMap} />
     </PageWrapper>
   );
 }
