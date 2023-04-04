a0338d28f408bedbd5dfe5631625b8a068836a4d
murraycopps <murraycopps@gmail.com>
2023-04-03 21:46:13 -0400

    styled single run card

--- components/StravaPage/SingleRunCard.tsx
+++ components/StravaPage/SingleRunCard.tsx
@@ -1,54 +1,110 @@
 import { outTime } from "@/scripts";
 import { Run } from "@/scripts/stravaTypes";
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 const MapWithNoSSR = dynamic(() => import("../Map"), {
-    ssr: false,
-  });
+  ssr: false,
+});
 
-  
 const SingleRunCard = ({ activity }: { activity: Run }) => (
-    <div className="fixed z-50 flex flex-col items-center justify-start gap-8 p-8 text-center rounded-lg shadow-lg inset-x-16 top-16 bg-faded-base-500">
-      <div className="flex flex-row justify-between w-full gap-8">
-        <h3 className="px-4 text-3xl truncate text-ellipsis max-w-3/4">
-          {activity.name}
-        </h3>
-        <p className="text-xl">
-          {new Date(activity.start_date_local).toLocaleDateString() +
-            " " +
-            new Date(activity.start_date_local).toLocaleTimeString([], {
-              hour: "numeric",
-              minute: "2-digit",
-            })}
-        </p>
+  <div className="fixed z-50 flex flex-col items-center justify-start gap-8 p-8 text-center rounded-lg shadow-lg inset-x-16s top-16s inset-16 bg-faded-base-500">
+    <div className="grid w-3/4 grid-cols-2 gap-8 place-items-center">
+      <h3 className="px-4 text-3xl">{activity.name}</h3>
+      <p className="text-xl">
+        {new Date(activity.start_date_local).toLocaleDateString() +
+          " " +
+          new Date(activity.start_date_local).toLocaleTimeString([], {
+            hour: "numeric",
+            minute: "2-digit",
+          })}
+      </p>
+    </div>
+    <div className="flex flex-row w-full gap-4">
+      <div className="grid w-56 gap-4 px-8 place-items-center">
+        <div className="flex flex-col items-center justify-start gap-2">
+          <p className="text-lg">Distance</p>
+          <p className="text-3xl">
+            {Math.round((activity.distance / 1609.34) * 100) / 100 || "--"}
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-start gap-2">
+          <p className="text-lg">Time</p>
+          <p className="text-3xl">{outTime(activity.moving_time) || "--"}</p>
+        </div>
+        <div className="flex flex-col items-center justify-start gap-2">
+          <p className="text-lg">Pace</p>
+          <p className="text-3xl">
+            {outTime(activity.moving_time / (activity.distance / 1609.34), 0) ||
+              "--"}
+          </p>
+        </div>
       </div>
       {activity.map.summary_polyline !== null &&
         activity.map.summary_polyline !== "" && (
-            <div className="relative w-2/3 overflow-hidden rounded-2xl">
-              <MapWithNoSSR map={activity.map} />
-            </div>
+          <div className="relative overflow-hidden grow h-fit rounded-2xl">
+            <MapWithNoSSR map={activity.map} />
+          </div>
         )}
-      <div className="grid w-full grid-cols-3 gap-4 px-8 place-items-center">
+      <div className="grid w-56 gap-4 px-8 place-items-center">
         <div className="flex flex-col items-center justify-start gap-2">
-          <p className="text-lg">Distance (Mi)</p>
+          <p className="text-lg">Average HeartRate</p>
+          <p className="text-3xl">{activity.average_heartrate || "--"}</p>
+        </div>
+        <div className="flex flex-col items-center justify-start gap-2">
+          <p className="text-lg">Max HeartRate</p>
+          <p className="text-3xl">{activity.max_heartrate || "--"}</p>
+        </div>
+        <div className="flex flex-col items-center justify-start gap-2">
+          <p className="text-lg">Average Speed</p>
           <p className="text-3xl">
-            {Math.round((activity.distance / 1609.34) * 100) / 100}
+            {Math.round(activity.average_speed * 2.23694 * 100) / 100 || "--"}
           </p>
         </div>
         <div className="flex flex-col items-center justify-start gap-2">
-          <p className="text-lg">Time (Mins)</p>
-          <p className="text-3xl">{outTime(activity.moving_time)}</p>
-        </div>
-        <div className="flex flex-col items-center justify-start gap-2">
-          <p className="text-lg">Pace (Mins/Mi)</p>
+          <p className="text-lg">Max Speed</p>
           <p className="text-3xl">
-            {outTime(activity.moving_time / (activity.distance / 1609.34), 0)}
+            {Math.round(activity.max_speed * 2.23694 * 100) / 100 || "--"}
           </p>
         </div>
-        <div className="grid w-full grid-cols-4"></div>
       </div>
     </div>
-  );
-  
+    <div className="grid w-full grid-cols-3 place-items-center">
+      <div className="grid w-full grid-cols-3 col-span-2">
+        <div className="flex flex-col items-center justify-start gap-2">
+          <p className="text-lg">Cadence</p>
+          <p className="text-3xl">{activity.average_cadence || "--"}</p>
+        </div>
+        <div className="flex flex-col items-center justify-start gap-2">
+          <p className="text-lg">Number of Steps</p>
+          <p className="text-3xl">
+            {activity.average_cadence
+              ? Math.round(
+                  (activity.average_cadence * activity.moving_time) / 30
+                )
+              : "--"}
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-start gap-2">
+          <p className="text-lg">Stride Length</p>
+          <p className="text-3xl">
+            {activity.average_cadence
+              ? Math.round(
+                  (activity.average_speed / (activity.average_cadence / 30)) *
+                    100
+                ) / 100
+              : "--"}
+          </p>
+        </div>
+      </div>
+      <Link
+        href={`/strava/activities/${activity.id}`}
+        className="w-full py-4 text-3xl rounded-full bg-wisteria-600 hover:bg-faded-lavender-600"
+      >
+        View More Stats
+      </Link>
+    </div>
+  </div>
+);
 
-  export default SingleRunCard;
+export default SingleRunCard;
