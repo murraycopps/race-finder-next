6b83695bf267bc0bddec364f77c741b9e0a59473
murraycopps <murraycopps@gmail.com>
2023-04-10 10:34:02 -0400

    added 2 more quotes

--- scripts/quotes.ts
+++ scripts/quotes.ts
@@ -4,13 +4,23 @@ const quotes:Quote[] = [
     {
         name: "Eliud Kipchoge",
         quote: "No Human is Limited",
-        img: "/images/quotes/Kip.jpeg"
+        img: "/images/quotes/kip.jpeg"
     },
     {
         name: "Steve Prefontaine",
         quote: "To give anything less than your best, is to sacrifice the gift.",
-        img: "/images/quotes/Pre.png"
-    }
+        img: "/images/quotes/pre.png"
+    },
+    {
+        name: "Kara Goucher",
+        quote: "I don't run to add days to my life. I run to add life to my days.",
+        img: "/images/quotes/kara.jpeg"
+    },
+    {
+        name: "Mo Farah",
+        quote: "Don't dream of winning, train for it!",
+        img: "/images/quotes/mo.jpeg"
+    },
     ];
 
     export {quotes};
