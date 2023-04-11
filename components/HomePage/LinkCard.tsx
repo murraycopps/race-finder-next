dfc3b2ddea0afefe3432d6fd6130c521529f419b
murraycopps <murraycopps@gmail.com>
2023-04-11 19:42:54 -0400

    fixed images

--- components/HomePage/LinkCard.tsx
+++ components/HomePage/LinkCard.tsx
@@ -11,16 +11,16 @@ type CardProps = {
   const LinkCard = ({ title, description, link, img }: CardProps) => (
     <Link
       href={link}
-      className="relative flex flex-col p-8 overflow-hidden gap-8 justify-evenly bg-lavender-600 home-link-card aspect-square card-slant"
+      className="relative flex flex-col gap-8 p-8 overflow-hidden justify-evenly bg-lavender-600 home-link-card aspect-square card-slant"
     >
       <Image
         src={img}
         alt=""
         width="1000"
         height="1000"
-        className="absolute inset-0 object-cover w-full h-full p-8"
+        className="absolute inset-0 object-cover w-full h-full py-8 px-14"
       />
-      <div className="absolute inset-0 w-full h-full bg-gray-600 opacity-75 h over:opacity-60" />
+      <div className="absolute inset-0 w-full h-full bg-gray-600 opacity-75 hover:opacity-60" />
       <h2 className="z-50 text-4xl font-bold pointer-events-none">{title}</h2>
       <p className="z-50 text-2xl pointer-events-none">{description}</p>
     </Link>
