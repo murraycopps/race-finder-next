484a765d728dd14392dfde9e398dfda3dd9ffd4b
murraycopps <murraycopps@gmail.com>
2023-04-24 08:15:07 -0400

    an option

--- pages/reviews/shoes/index.tsx
+++ pages/reviews/shoes/index.tsx
@@ -11,9 +11,11 @@ export default function Reviews() {
       page="Reviews"
       className="flex flex-col items-center justify-start w-screen h-screen gap-4 p-10 overflow-y-auto"
     >
-      <h1 className="w-full p-4 mt-8 text-4xl font-medium text-center text-white slanted bg-ronchi-600">
+      {/* <h1 className="w-full p-4 mt-8 text-4xl font-medium text-center text-white slanted bg-ronchi-600">
         Shoe Reviews
-      </h1>
+      </h1> */}
+      <h1 className="mt-8 text-6xl text-slate-400">Clothes Reviews</h1>
+
       <Filter setFilteredShoes={setFilteredShoes} />
       <div className="grid justify-start w-full grid-cols-1 gap-4 p-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
         {filteredShoes.map((shoe, i) => (
