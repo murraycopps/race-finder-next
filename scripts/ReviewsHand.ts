e6f481ee1d3aaebc09e47b5f9dc1326bf3aeba08
torstenbrickley <torstenbrickley@oldrochester.org>
2023-04-25 10:51:58 -0400

    create revievw started

---
+++ scripts/ReviewsHand.ts
@@ -0,0 +1,27 @@
+import {Review} from "@/scripts/types";
+
+export default class ReviewsHand {
+    static hasReview = false;
+
+    static async getReviews() {
+        if(this.hasReview) {
+            return [];
+        }
+        this.hasReview = true;
+        const reviews = await fetch('/api/reviews');
+        const reviewsJson = await reviews.json();
+        return reviewsJson;
+    }
+
+    static async postReview(review: Review) {
+const response = await fetch('/api/reviews', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(review),
+        });
+        const json = await response.json();
+        return json;
+
+    }}
