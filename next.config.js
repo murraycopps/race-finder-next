fb84c1e66d16baee99576da237675f27d51f11a1
murraycopps <murraycopps@gmail.com>
2023-09-20 09:14:56 -0400

    changed config

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         port: '',
         pathname: '/pictures/athletes/*/*/*/*',
       },
-    //                       src="https://media.licdn.com/dms/image/C4E0BAQHBmWI9w3tzog/company-logo_200_200/0/1544572615288?e=2147483647&v=beta&t=HhwAog-YBLZyc1ULuVv48MZefPUw3EHejmW25OtMwUQ"
+      //                       src="https://media.licdn.com/dms/image/C4E0BAQHBmWI9w3tzog/company-logo_200_200/0/1544572615288?e=2147483647&v=beta&t=HhwAog-YBLZyc1ULuVv48MZefPUw3EHejmW25OtMwUQ"
       {
         protocol: 'https',
         hostname: 'media.licdn.com',
@@ -25,16 +25,23 @@ const nextConfig = {
     MONGODB_URI: process.env.MONGODB_URI,
   },
 
+  build: {
+    env: {
+      STRAVA_CLIENT_ID: process.env.STRAVA_CLIENT_ID,
+      STRAVA_CLIENT_SECRET: process.env.STRAVA_CLIENT_SECRET,
+      MONGODB_URI: process.env.MONGODB_URI,
+    }
+  },
   webpack: (config, isServer) => {
-    config.resolve.fallback = { 
+    config.resolve.fallback = {
       tls: false,
       fs: false,
       net: false,
     };
 
 
-      return config;
-    }
+    return config;
   }
+}
 
 module.exports = nextConfig
