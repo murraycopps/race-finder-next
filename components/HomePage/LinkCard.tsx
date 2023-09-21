a2635241cab449720a6e389f5c00434f018c4f8f
murraycopps <murraycopps@gmail.com>
2023-09-20 22:20:15 -0400

    safari doesnt have aspect ratio

--- components/HomePage/LinkCard.tsx
+++ components/HomePage/LinkCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 type CardProps = {
   title: string;
@@ -8,10 +9,19 @@ type CardProps = {
   img: string;
 };
 
-const LinkCard = ({ title, description, link, img }: CardProps) => (
+const LinkCard = ({ title, description, link, img }: CardProps) => {
+  const ref = useRef<HTMLAnchorElement>(null);
+  useEffect(() => {
+    if (ref.current) {
+      const width = ref.current.offsetWidth;
+      ref.current.style.height = `calc(${width}px - 3rem)`;
+    }
+  }, [ref]);
+  return (
   <Link
     href={link}
-    className="relative flex flex-col gap-8 p-8 overflow-hidden justify-evenly bg-lavender-600 home-link-card aspect-square card-slant"
+    className="relative flex flex-col gap-8 p-8 overflow-hidden justify-evenly bg-lavender-600 card-slant"
+    ref={ref}
   >
     <Image
       src={img}
@@ -25,6 +35,6 @@ const LinkCard = ({ title, description, link, img }: CardProps) => (
     <h1 className="z-50 text-6xl font-semibold pointer-events-none">{title}</h1>
     <p className="z-50 text-2xl pointer-events-none">{description}</p>
   </Link>
-);
+)};
 
 export default LinkCard;
