ceed0455b8d78a26e80362829a922807f323c248
murraycopps <murraycopps@gmail.com>
2024-01-24 15:24:35 -0500

    fixed? pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -32,68 +34,25 @@ const nextConfig = {
       MONGODB_URI: process.env.MONGODB_URI,
     }
   },
-  webpack: (config, isServer) => {
+  webpack: (config, { isServer }) => {
     config.resolve.fallback = {
       tls: false,
       fs: false,
       net: false,
     };
 
-
     return config;
-  }
+  },
+
+  ...withPWA({
+    pwa: {
+      dest: 'public',
+      disable: process.env.NODE_ENV === 'development',
+      register: true,
+      skipWaiting: true,
+    },
+  }),
+
 }
 
-module.exports = nextConfig
-
-const withPWA = require("next-pwa");
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    disable: process.env.NODE_ENV === 'development',
-    skipWaiting: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'dgalywyr863hv.cloudfront.net',
-        port: '',
-        pathname: '/pictures/athletes/*/*/*/*',
-      },
-      //                       src="https://media.licdn.com/dms/image/C4E0BAQHBmWI9w3tzog/company-logo_200_200/0/1544572615288?e=2147483647&v=beta&t=HhwAog-YBLZyc1ULuVv48MZefPUw3EHejmW25OtMwUQ"
-      {
-        protocol: 'https',
-        hostname: 'media.licdn.com',
-        port: '',
-        pathname: '/dms/image/*/*/*/*',
-      }
-    ]
-  },
-
-  env: {
-    STRAVA_CLIENT_ID: process.env.STRAVA_CLIENT_ID,
-    STRAVA_CLIENT_SECRET: process.env.STRAVA_CLIENT_SECRET,
-    MONGODB_URI: process.env.MONGODB_URI,
-  },
-
-  build: {
-    env: {
-      STRAVA_CLIENT_ID: process.env.STRAVA_CLIENT_ID,
-      STRAVA_CLIENT_SECRET: process.env.STRAVA_CLIENT_SECRET,
-      MONGODB_URI: process.env.MONGODB_URI,
-    }
-  },
-  webpack: (config, isServer) => {
-    config.resolve.fallback = {
-      tls: false,
-      fs: false,
-      net: false,
-    };
-
-
-    return config;
-  }
-
-});
+module.exports = nextConfig;
