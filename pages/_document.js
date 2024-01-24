282c2ee1e533781b29a33b8e8f7e7896543ea518
murraycopps <murraycopps@gmail.com>
2024-01-24 16:00:00 -0500

    i was wrong

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,21 @@
-import { Html, Head, Main, NextScript } from "next/document";
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="manifest" href="/manifest/manifest.webmanifest"></link>
-        <link rel="apple-touch-icon" href="/manifest/icon-192x192.png"></link>
-        <meta name="theme-color" content="#0c0521" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest/manifest.webmanifest" />
+          <link rel="apple-touch-icon" href="/manifest/icon-192x192.png" />
+          <meta name="theme-color" content="#0c0521" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
