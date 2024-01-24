b2ef5d0fc21fdbf59e75c596770c9ea38745dd40
murraycopps <murraycopps@gmail.com>
2024-01-24 16:26:55 -0500

    service worker couldnt be in scripts folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
-        .register("@/scripts/service-worker.js")
+        .register("/service-worker.js")
         .then((registration) => {
           console.log(
             "Service Worker registered with scope:",
