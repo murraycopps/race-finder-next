dfa6789257eafdcca729102e421308383d0c2b03
murraycopps <murraycopps@gmail.com>
2024-01-26 20:59:56 -0500

    added better navigation and hid logout button

--- components/PageWrapper/NavbarContent.tsx
+++ components/PageWrapper/NavbarContent.tsx
@@ -1,7 +1,9 @@
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import Link from "next/link";
 import NavbarDropdown from "./NavbarDropdown";
 import { Route } from "./types";
+import LoginData from "@/scripts/LoginData";
+import { useRouter } from "next/router";
 
 const routes: Route[] = [
   {
@@ -48,6 +50,19 @@ const routes: Route[] = [
 
 const NavbarContent = forwardRef<HTMLDivElement, { open: boolean }>(
   function NavbarContent(props, ref) {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const router = useRouter();
+    const route = router.route.replace("[pid]", router.query.pid as string);
+
+    useEffect(() => {
+      const checkIfLoggedIn = async () => {
+        await LoginData.getStorage();
+        setLoggedIn(LoginData.isLoggedIn());
+      };
+      checkIfLoggedIn();
+    }, []);
+
     return (
       <nav
         className={`lg:w-1/4 md:w-1/3 sm:w-1/2 w-screen z-100 fixed top-0 left-0 bottom-0 nav-bar-content bg-base-700 transition-all-300 pt-14 ${
@@ -71,6 +86,23 @@ const NavbarContent = forwardRef<HTMLDivElement, { open: boolean }>(
         >
           Strava
         </Link>
+        {loggedIn && (
+          <div className="absolute w-full bottom-10">
+            <button
+              onClick={() => {
+                LoginData.Logout();
+                if (route.includes("strava")) {
+                  router.push("/home");
+                } else {
+                  router.reload();
+                }
+              }}
+              className="block w-3/4 h-10 px-6 mx-auto text-lg place-items-center rounded-xl hover:rounded-3xl transition-all-300 bg-rose-500 hover:bg-rose-600"
+            >
+              Logout
+            </button>
+          </div>
+        )}
       </nav>
     );
   }
--- components/PageWrapper/index.tsx
+++ components/PageWrapper/index.tsx
@@ -1,87 +1,117 @@
 import Head from "next/head";
-import {HTMLAttributes, useEffect, useState} from "react";
+import { HTMLAttributes, useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import LoginData from "@/scripts/LoginData";
 import Link from "next/link";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 interface PageWrapperProps extends HTMLAttributes<HTMLDivElement> {
-    page: string;
-    loginPage?: boolean;
+  page: string;
+  loginPage?: boolean;
 }
 
 const generateRoute = (route: string | string[] | undefined) => {
-    if(typeof route === "string"){
-        if(route.includes("strava")){
-            return "/home"
-        }
-
-        //   check if route is a valid route
-        if(route.charAt(0) === "/") return route
+  if (typeof route === "string") {
+    if (route.includes("strava")) {
+      return "/home";
     }
-    return "/home"
-}
+
+    //   check if route is a valid route
+    if (route.charAt(0) === "/") return route;
+  }
+  return "/home";
+};
 
 export default function PageWrapper(props: PageWrapperProps) {
-    const router = useRouter();
-    const route = router.route.replace("[pid]", router.query.pid as string)
-    const [loggedIn, setLoggedIn] = useState(false);
-    useEffect(() => {
-        const checkIfLoggedIn = async () => {
-            await LoginData.getStorage();
-            setLoggedIn(LoginData.isLoggedIn());
-        };
-        checkIfLoggedIn();
-    }, []);
+  const router = useRouter();
+  const route = router.route.replace("[pid]", router.query.pid as string);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [forward, setForward] = useState("");
+  useEffect(() => {
+    const checkIfLoggedIn = async () => {
+      await LoginData.getStorage();
+      setLoggedIn(LoginData.isLoggedIn());
+    };
+    checkIfLoggedIn();
+  }, []);
 
-
-    return (
-        <>
-            {/* breakpoints */}
-             {/* <div className="fixed top-0 right-0 w-10 h-10 bg-red-500 border-2 z-110 sm:bg-green-500 md:bg-blue-600 lg:bg-lavender-600 border-base" /> */}
-            <Head>
-                <title>{props.page}</title>
-            </Head>
-            <header>
-                <Navbar/>
-                {!route.includes("login") && !route.includes("create-account") ? (
-                        <div className="relative">
-                            {loggedIn ? (
-                                <div className="flex justify-end">
-                                    <button onClick={() => {
-                                        LoginData.Logout();
-                                        if (route.includes("strava")) {
-                                            router.push("/home");
-                                        } else {
-                                            router.reload();
-                                        }
-                                    }}
-                                            className="fixed grid h-10 px-6 text-lg transform -translate-y-1/2 place-items-center right-4 rounded-xl hover:rounded-3xl transition-all-300 bg-rose-500 hover:bg-rose-600 z-100 top-10 ">
-                                        Logout
-                                    </button>
-                                </div>
-                            ) : (!props.loginPage && (
-                                <Link
-                                    href={`/login${route ? `?route=${route}` : ""}`}
-                                    className="fixed grid h-10 px-6 text-lg transform -translate-y-1/2 place-items-center right-4 rounded-xl hover:rounded-3xl transition-all-300 bg-rose-500 hover:bg-rose-600 z-100 top-10 "
-                                >
-                                    Login
-                                </Link>
-                            ))}
-
-                        </div>
-                    ) : (
-                            <Link
-                                href={generateRoute(router.query.route)}
-                                className="absolute grid h-10 px-6 text-lg transform -translate-y-1/2 place-items-center right-4 rounded-xl hover:rounded-3xl transition-all-300 bg-rose-500 hover:bg-rose-600 z-100 top-10 "
-                            >
-                                Back
-                            </Link>
-                    )
-
-                }
-            </header>
-            <main {...props} />
-        </>
-    );
+  return (
+    <>
+      {/* breakpoints */}
+      {/* <div className="fixed top-0 right-0 w-10 h-10 bg-red-500 border-2 z-110 sm:bg-green-500 md:bg-blue-600 lg:bg-lavender-600 border-base" /> */}
+      <Head>
+        <title>{props.page}</title>
+      </Head>
+      <header>
+        <Navbar />
+        {!route.includes("login") && !route.includes("create-account") ? (
+          <div className="relative">
+            {loggedIn ? (
+              <div className="absolute grid w-20 h-10 grid-cols-2 gap-4 text-white z-100 top-2 right-5 sm:hidden">
+                <button onClick={() => router.back()}>
+                  <svg
+                    className={`w-full h-full rotate-90`}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M7 10L12 15L17 10"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+                <button onClick={() => {
+                    window.history.forward()
+                }}>
+                  <svg
+                    className={`w-full h-full -rotate-90`}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M7 10L12 15L17 10"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+              </div>
+            ) : (
+              !props?.loginPage && (
+                <Link
+                  href={`/login${route ? `?route=${route}` : ""}`}
+                  className="fixed grid h-10 px-6 text-lg transform -translate-y-1/2 place-items-center right-4 rounded-xl hover:rounded-3xl transition-all-300 bg-rose-500 hover:bg-rose-600 z-100 top-10 "
+                >
+                  Login
+                </Link>
+              )
+            )}
+            {/* {!loggedIn && (
+              <Link
+                href={`/login${route ? `?route=${route}` : ""}`}
+                className="fixed grid h-10 px-6 text-lg transform -translate-y-1/2 place-items-center right-4 rounded-xl hover:rounded-3xl transition-all-300 bg-rose-500 hover:bg-rose-600 z-100 top-10 "
+              >
+                Login
+              </Link>
+            )} */}
+          </div>
+        ) : (
+          <Link
+            href={generateRoute(router.query.route)}
+            className="absolute grid h-10 px-6 text-lg transform -translate-y-1/2 place-items-center right-4 rounded-xl hover:rounded-3xl transition-all-300 bg-rose-500 hover:bg-rose-600 z-100 top-10 "
+          >
+            Back
+          </Link>
+        )}
+      </header>
+      <main {...props} />
+    </>
+  );
 }
